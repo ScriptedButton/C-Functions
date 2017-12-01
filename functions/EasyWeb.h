1428295d12873f784819a0f5c37e1a4dfcf0458f
ScriptedButton <33437875+ScriptedButton@users.noreply.github.com>
2017-11-30 21:17:46 -0500

    Create EasyWeb.h
    You can call the function by using WebRequest(url);

---
+++ functions/EasyWeb.h
@@ -0,0 +1,41 @@
+// WebReq Function - Call using WebRequest(url here); - Lenin
+
+#pragma once
+#pragma comment(lib, "wininet.lib")
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define NOMINMAX
+#include <Windows.h>
+#include <WinInet.h>
+#include <string>
+using namespace std;
+string WebRequest(string url) {
+	string result;
+	HINTERNET connect = InternetOpen("MyBrowser", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+
+	if (!connect) {
+		cout << "Connection Failed or Syntax error";
+		return 0;
+	}
+
+	HINTERNET OpenAddress = InternetOpenUrl(connect, url.c_str(), NULL, 0, INTERNET_FLAG_PRAGMA_NOCACHE | INTERNET_FLAG_KEEP_CONNECTION, 0);
+
+	if (!OpenAddress)
+	{
+		DWORD ErrorNum = GetLastError();
+		cout << "Failed to open URL \nError No: " << ErrorNum;
+		InternetCloseHandle(connect);
+		return 0;
+	}
+
+	char DataReceived[4096] = { 0 };
+	DWORD NumberOfBytesRead = 0;
+	while (InternetReadFile(OpenAddress, DataReceived, 4096, &NumberOfBytesRead) && NumberOfBytesRead)
+	{
+		result += DataReceived;
+	}
+
+	InternetCloseHandle(OpenAddress);
+	InternetCloseHandle(connect);
+
+	return result;
+}
